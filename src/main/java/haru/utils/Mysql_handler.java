1ca1ccebc44b290b0a24b96a47a64e935c74074c
harumonia <zxjlm233@163.com>
2019-11-24 19:09:14 +0800

    sql change v2.0

--- src/main/java/haru/utils/Mysql_handler.java
+++ src/main/java/haru/utils/Mysql_handler.java
@@ -12,10 +12,15 @@ import java.util.List;
 import haru.models.User;
 import haru.view.MainFrame;
 
+//重要：sql连接的具体阐释，见sql_query_name()函数
+
 public class Mysql_handler {
 	
 	public static ResultSet sql_query(String sql) {
-		// TODO Auto-generated method stub
+		/*
+		 * sql查询，查询的结果会以ResultSet结构返回，可能会出现bug，已经弃用
+		 * 这里保留是用作反面教材，与sql_query_name和login_validate对照
+		 * */
 		Connection con = null;
 		ResultSet rs = null;
 		try {
@@ -25,10 +30,7 @@ public class Mysql_handler {
 			PreparedStatement ps = con.prepareStatement(sql);
 			
 			rs = ps.executeQuery();
-			
-			
 
-			
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -49,7 +51,13 @@ public class Mysql_handler {
 	
 	
 	public static Boolean sql_execute(String sql) {
-		// TODO Auto-generated method stub
+		/*
+		 * sql执行，传入的sql为标准的sql语句
+		 * 返回布尔值，表示操作成功或者失败
+		 * 
+		 * 注意：查询语句与操作语句分用两个函数，因为查询返回的是结果，操作返回的是操作是否完成，注意两者的区别。
+		 * */
+		
 		Connection con = null;
 		Boolean rs = true;
 		try {
@@ -85,17 +93,31 @@ public class Mysql_handler {
 	
 	
 	public static List<String> sql_query_name() {
-		// TODO Auto-generated method stub
+		/*
+		 * 查询sql的login_name属性的所有值，用来形成一个name_list,用来比对注册请求中是否会出现重复的用户名
+		 * 
+		 * 注意：这里当然可以通过给login_name赋unique属性，来使得login_name唯一，不过这样错误发生在远程数据库，并且增加了本地程序和远程数据库的交互次数，所以选择获取login_name列表，在本地进行约束
+		 * 当然你还可以为login_name建立一个全局变量，这样可以进一步地减少交互
+		 * */
 		Connection con = null;
 		ResultSet rs = null;
 		List<String> name_l = new ArrayList<String>();
 		try {
+			// MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://39.108.229.166:3306/Java_sql?useSSL=false","root","root");
+			
+			// MySQL 8.0 以下版本 - JDBC 驱动名及数据库 URL
+//			Class.forName("com.mysql.jdbc.Driver");
+//			con = DriverManager.getConnection("jdbc:mysql://39.108.229.166:3306/Java_sql?useSSL=false","root","root");
 
 			PreparedStatement ps = con.prepareStatement("select login_name from user");
+			//预编译sql语句
 			
 			rs = ps.executeQuery();
+			//执行sql语句，将查询的结果存放在rs中
+			//每次连接数据库，都要记得关闭连接；而在连接关闭之后，rs的生命周期结束，所以试图返回rs是不可能的
+			//于是我使用了一个list，来存放查询的结果，list的生命周期是可以由我自己决定的
 			
 			while(rs.next()) {
 				String name_tmp = rs.getString("login_name");
@@ -103,7 +125,6 @@ public class Mysql_handler {
 			}
 			
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -111,8 +132,8 @@ public class Mysql_handler {
 			if (con!=null) {
 				try {
 					con.close();
+					//关闭与数据库的连接
 				} catch (SQLException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
@@ -122,7 +143,11 @@ public class Mysql_handler {
 	
 	
 	public static Boolean login_validate(String name,String pwd) {
-		// TODO Auto-generated method stub
+		/*
+		 * 用来验证登录操作是否能够成功
+		 * 传入login_name和password两个属性，用来查询验证
+		 * 返回是否允许登录
+		 * */
 		Connection con = null;
 		ResultSet rs = null;
 		Boolean res = false;
